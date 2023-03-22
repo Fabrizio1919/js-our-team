72e9973d6078b44542cf28f33762e0871dcd2530
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-22 15:03:16 +0100

    Structure with card add for and link markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,36 +2,56 @@ const team = [
     {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
-        img: '',
+        img: 'wayne-barnett-founder-ceo.jpg',
     },
     {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
-        img: '',
+        img: 'angela-caroll-chief-editor.jpg',
     },
     {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
-        img: '',
+        img: 'walter-gordon-office-manager.jpg',
     },
     {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
-        img: '',
+        img: 'angela-lopez-social-media-manager.jpg',
     },
     {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
-        img: '',
+        img: 'scott-estrada-developer.jpg',
     },
-     {
+    {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
-        img: '',
+        img: 'barbara-ramos-graphic-designer.jpg',
     },
 ]
 console.log(team);
+const rowEl = document.querySelector('.row')
 for (let i = 0; i < team.length; i++) {
     const thisteam = team[i];
     console.log(thisteam);
+    console.log(thisteam.nome);
+    console.log(thisteam.ruolo);
+    const markup = `
+    <div class="col">
+        <div class="card">
+            <img src="./assets/img/${thisteam.img}" alt="" class="card-img-top">
+            <div class="card-body">
+                <h3>
+                    ${thisteam.nome} 
+                </h3>
+                <p>
+                    ${thisteam.ruolo}
+                </p>
+            </div>
+            
+        </div>
+    </div>`
+    rowEl.innerHTML += markup
 }
+
