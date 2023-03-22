62bb9f53f30aa3b9540db57f6678e793aea3d5ad
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-22 14:17:57 +0100

    Structure Html Css JS + structure team in Object array

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        img: '',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        img: '',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        img: '',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        img: '',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        img: '',
+    },
+     {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        img: '',
+    },
+]
+console.log(team);
+for (let i = 0; i < team.length; i++) {
+    const thisteam = team[i];
+    console.log(thisteam);
+}
